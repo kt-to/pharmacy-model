bb3dce7466378076e09cc3a42f1c82f960a9bcd1
Kto-_-to <fomiknihdima@gmail.com>
2024-10-07 11:54:33 +0300

    include medicament.h

---
+++ person.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Дмитрий Фоминых on 07.10.2024.
+//
+//#pragma once
+#include "person.h"
+#include "medicament.h"
+
+person::person() {
+    _name = "Ivan";
+    _surname = "Grozniy";
+
+}

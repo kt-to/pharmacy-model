493afb368da66c7b7e715b4d34da94cd83661767
Kto-_-to <fomiknihdima@gmail.com>
2024-10-07 12:23:11 +0300

    add person constructor

--- person.cpp
+++ person.cpp
@@ -4,9 +4,18 @@
 //#pragma once
 #include "person.h"
 #include "medicament.h"
+#include "random"
 
 person::person() {
-    _name = "Ivan";
-    _surname = "Grozniy";
-
+    std::mt19937 rd;
+    _gender = (rd() % 2 == 1);
+    if (_gender) {
+        _name = gen_man();
+    } else {
+        _name = gen_women();
+    }
+    _surname = gen_surname();
+    _age = 16 + (rd() % 74);
+    _med_card = {};
+    _therapy = {};
 }

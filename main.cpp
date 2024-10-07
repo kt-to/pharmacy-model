2cdb4d7d46c46a6c30d14da6892ad627f4863263
Kto-_-to <fomiknihdima@gmail.com>
2024-10-07 15:59:45 +0300

    fix immage

--- main.cpp
+++ main.cpp
@@ -10,14 +10,17 @@
 
 int main(int argc, char* argv[])
 {
-//    QApplication a(argc, argv);
-//    std::string s = *argv;
-//    mainwindow w(s);
-//    w.show();
-//    return QApplication::exec();
+    QApplication a(argc, argv);
+    std::string s = *argv;
+    mainwindow w(s);
+    w.show();
 
     for (int i = 0; i < 10; i++) {
         auto now = new person;
         now->print();
     }
+
+    return QApplication::exec();
+
+
 }

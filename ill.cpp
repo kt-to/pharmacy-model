0b3919441f8aec59210f18c38d5bc32d40e23a16
Kto-_-to <fomiknihdima@gmail.com>
2024-10-07 11:28:18 +0300

    start work in ill

---
+++ ill.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Дмитрий Фоминых on 07.10.2024.
+//
+
+enum class ill {
+    prevention,
+    heart,
+    asma,
+    intestine,
+    psychological,
+    primary_care,
+    sleeping_pills,
+    antibiotics,
+    antiretroviral,
+    antiviral_drugs,
+    vaccines
+};
